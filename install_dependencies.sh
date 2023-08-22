29c5408f931a00d0d5a91770a38478bd3a956518
Keegan Sanchez <keegan.sachez@wsu.edu>
2023-08-22 12:49:47 -0700

    Added luacheck

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt install git python3 python3-dev make gcc libncurses-dev cmake golang mono-complete nodejs npm default-jdk -y
+sudo apt install lua-check git python3 python3-dev make gcc libncurses-dev cmake golang mono-complete nodejs npm default-jdk -y
